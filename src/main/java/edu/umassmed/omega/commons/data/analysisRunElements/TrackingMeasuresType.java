317c1fdde6609c26648bc18d6d0e2a2478352dfb
akramhar <alex.83@bluewin.ch>
2016-08-22 15:38:16 -0400

    project structure update

---
+++ src/main/java/edu/umassmed/omega/commons/data/analysisRunElements/TrackingMeasuresType.java
@@ -0,0 +1,5 @@
+package edu.umassmed.omega.commons.data.analysisRunElements;
+
+public enum TrackingMeasuresType {
+	Intensity, Velocity, Mobility, Diffusivity
+}
---
+++ src/main/java/edu/umassmed/omega/commons/data/coreElements/OmeroElement.java
@@ -0,0 +1,8 @@
+package edu.umassmed.omega.commons.data.coreElements;
+
+public interface OmeroElement {
+
+	public void setOmeroId(Long omeroId);
+
+	public Long getOmeroId();
+}
